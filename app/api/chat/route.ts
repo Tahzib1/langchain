b0b129e82f1c26ce73707569171e70eabd11fb7e
Tahzib1 <tahzib.sunesara@criticalmass.com>
2025-01-13 11:49:31 -0600

    update to use aws db instead of planetscale

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -6,10 +6,6 @@ import { SqlDatabase } from "langchain/sql_db";
 import { SqlDatabaseChain } from "langchain/chains/sql_db";
 import { DataSource } from "typeorm";
 
-const formatMessage = (message: VercelChatMessage) => {
-  return `${message.role}: ${message.content}`;
-};
-
 const template = `Given an input question, first create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
 Use the following format:
 
@@ -27,7 +23,7 @@ Question: {input}`;
 const isProd = process.env.NODE_ENV === "production";
 
 const datasource = new DataSource({
-  type: 'mysql',
+  type: "mysql",
   host: process.env.DATABASE_HOST,
   port: 3306,
   username: process.env.DATABASE_USERNAME,
@@ -35,7 +31,7 @@ const datasource = new DataSource({
   database: process.env.DATABASE_NAME,
   ssl: {
     rejectUnauthorized: false,
-    ca: isProd ? process.env.PLANETSCALE_SSL_CERT_PATH : '',
+    // ca: isProd ? process.env.PLANETSCALE_SSL_CERT_PATH : "",
   },
 });
 
@@ -43,30 +39,38 @@ export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
     const messages = body.messages ?? [];
-    const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
     const currentMessageContent = messages[messages.length - 1].content;
 
     const prompt = PromptTemplate.fromTemplate(template);
-    
+
     const db = await SqlDatabase.fromDataSourceParams({
       appDataSource: datasource,
+      includesTables: [
+        "film",
+        "actor",
+        "film_actor",
+        "category",
+        "film_category",
+        "film_text",
+      ],
     });
-    const model = new OpenAI({ temperature: 0, openAIApiKey: process.env.OPENAI_API_KEY });
-    
+    const model = new OpenAI({
+      temperature: 0,
+      openAIApiKey: process.env.OPENAI_API_KEY,
+    });
+
     const chain = new SqlDatabaseChain({
       llm: model,
       database: db,
       sqlOutputKey: "sql",
       prompt,
-      // verbose: true
     });
 
     const res = await chain.invoke({
       query: currentMessageContent,
     });
 
-    return NextResponse.json(res, { status: 200});
-
+    return NextResponse.json(res, { status: 200 });
   } catch (e: any) {
     return NextResponse.json({ error: e.message }, { status: 500 });
   }
--- components/chat/ChatWindow.tsx
+++ components/chat/ChatWindow.tsx
@@ -1,51 +1,71 @@
-"use client"
+"use client";
 
-import { useChat } from 'ai/react';
+import { useChat } from "ai/react";
 
 import ChatForm from "./ChatForm";
 import ChatBubble from "./ChatBubble";
 
-
 const ChatWindow = () => {
-
-  const { messages, input, handleInputChange, handleSubmit, isLoading, append } = useChat({
-    initialMessages: [{id: '0', role: 'assistant', content: '{ "result": "Hi, I am an AI Assistant Trained to answer questions about a SQL database. How can I help?", "sql": ""}'}],
+  const {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+    isLoading,
+    append,
+  } = useChat({
+    initialMessages: [
+      {
+        id: "0",
+        role: "assistant",
+        content:
+          '{ "result": "Hi, I am an AI Assistant Trained to answer questions about a SQL Movie database. How can I help?", "sql": ""}',
+      },
+    ],
     onResponse(response) {
-      console.log(response);  
+      console.log(response);
     },
     onError: (e) => {
       console.error(e);
-    }
-  
+    },
   });
 
   const handleSuggestedPromptsClick = (text: string) => {
-    append({content: text, role: 'user', id: '' + messages.length + 1});
-  }
+    append({ content: text, role: "user", id: "" + messages.length + 1 });
+  };
 
   return (
     <div className="h-full flex flex-col bg-zinc-800">
       <div className="bg-black h-[90%] p-4 overflow-y-auto">
-          {messages.map((message, index) => {
-            const isAi = message.role === "system" || message.role === "assistant";
-            const jsonData = isAi && JSON.parse(message.content);
+        {messages.map((message, index) => {
+          const isAi =
+            message.role === "system" || message.role === "assistant";
+          const jsonData = isAi && JSON.parse(message.content);
 
-            return <ChatBubble 
-              key={message.id} 
-              content={jsonData.result || message.content} 
-              sql={jsonData.sql || ''} role={message.role} 
-              isFirstMessage={index === 0} 
+          return (
+            <ChatBubble
+              key={message.id}
+              content={jsonData.result || message.content}
+              sql={jsonData.sql || ""}
+              role={message.role}
+              isFirstMessage={index === 0}
               handleSuggestedPromptsClick={handleSuggestedPromptsClick}
               isLoading={isLoading}
             />
-          })}
+          );
+        })}
       </div>
 
       <div className="w-11/12 h-[10%] mx-auto mt-4">
-        <ChatForm handleSubmit={handleSubmit} handleInputChange={handleInputChange} inputVal={input} isLoading={isLoading} />
+        <ChatForm
+          handleSubmit={handleSubmit}
+          handleInputChange={handleInputChange}
+          inputVal={input}
+          isLoading={isLoading}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ChatWindow;
